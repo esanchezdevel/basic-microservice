b258add642aa63dc56ebc767bc4583dcd88c0d24
enrique.sanchez <enrique.sanchez@hpe.com>
2026-02-24 18:04:43 +0100

    feat: create project basic-microservice
    Create project basic-microservice that will be used to practice
    microservices implementation with Spring Boot.

---
+++ src/main/java/com/hpe/intam/vp/automation/BasicMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.hpe.intam.vp.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicMicroserviceApplication.class, args);
+	}
+
+}
